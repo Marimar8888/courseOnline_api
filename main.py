9ff2b444d26d606591bf3924b4ed899f89cc09b4
Marimar8888 <alonsomarimar@hotmail.com>
2024-10-17 07:34:31 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from app import create_app
+import requests
+
+app = create_app()
+
+@app.route('/') # Creo la ruta de acceso a nuestra aplicación con el decorador
+def hello():
+    return "Hey Flask"
+
+@app.route('/get-ip')
+def get_ip():
+    try:
+        # Consulta a un servicio externo para obtener la IP
+        response = requests.get('https://ifconfig.me')
+        return f"Your public IP is: {response.text}"
+    except Exception as e:
+        return f"Error getting IP: {str(e)}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
